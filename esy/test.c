a0300be9364d61a7ce0f2d687ad99b48562326c5
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2019-07-02 16:24:13 -0700

    just set more paths

--- esy/test.c
+++ esy/test.c
@@ -1,8 +1,4 @@
-/* https://metacpan.org/pod/distribution/EV/libev/ev.pod */
-#define EV_STANDALONE              /* keeps ev from requiring config.h */
-#define EV_SELECT_IS_WINSOCKET 1   /* configure libev for windows select */
-
-#include "ev.h"
+#include <ev.h>
 
 int main(int arc, char *argv[])
 {
--- esy/test.sh
+++ esy/test.sh
@@ -10,20 +10,7 @@ fi
 
 echo "Using compiler: $CC"
 
-echo "include..."
-#ls -a $INCLUDE
-echo "lib.."
-export LIBEV_LIB_PATH=$(echo ${LIBEV_LIBS} | cut -c 3- | awk '{print $1}')
-
 $CC ./../../esy/test.c -o ./test.exe $LIBEV_CFLAGS $LIBEV_LIBS
-
-# export PATH=$PATH:$cur__include:$cur__lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBEV_LIB_PATH
-
-#echo "Augmented path: $PATH"
-
-echo "Copying binaries..."
-#cp $cur__bin/*.dll .
 
 echo "Test executable path:"
 ls -a .
